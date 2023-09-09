a385bc62903f8d1f42c4d0c1f54060378ef8e012
ed.pecson <edpecson@Bricolage.lan>
2023-09-09 09:06:23 -0500

    Added client/server directories

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, {useState} from 'react';
+import './App.css';
+import styles from "./App.css?inline";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+  const [response, setResponse]=useState('');
 
-  return (
-    <>
+  const host = import.meta.env.VITE_HOST
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const node_url = `http://${host}:3001`
+    console.log(node_url)
+    fetch(node_url,{
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({message}),
+    })
+    .then((res) => res.json())
+    .then((data) => setResponse(data.message));
+  };
+
+
+
+  // Function to split response before the number and add <br /> elements
+  const formatResponse = (text) => {
+    // const sentences = text.split(/(?=\d+\.)/) || text.split(/Part/) || text.split('\n') ; // Split based on a number followed by a period
+    const sentences =  text.split('\n') ; 
+
+    // Map the sentences and add <br /> elements between them
+    const formattedResponse = sentences.map((sentence, index) => (
+      <span key={index}>
+        {sentence.trim()}<br />
+      </span>
+    ));
+
+    return formattedResponse;
+  };
+
+return (
+  <div>
+    <div>
+      <title>ML Strategies</title>
+      <link rel="icon" href="/chatbot.png" />
+    </div>
+
+    <main className={styles.main}>
+      <img src="/chatbot.png" className={styles.icon} />
+      <h3>What ways can ML help your business?</h3>
+                          <form onSubmit={handleSubmit}>
+                            <textarea
+                              rows="4"
+                              cols="50"
+                              type="text"
+                              name="user"
+                              placeholder="Describe your challenges"
+                              value={message}
+                              onChange={(e) => setMessage(e.target.value)}
+                            />
+                            <br></br>
+                            <input type="submit" value="Submit" />
+                          </form>
+                          </main>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <br></br>
+      {response && <div className="display-linebreak"> 
+      <b> Action Plan: </b> {formatResponse(response)}</div>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+  
+  </div>
+);
 }
 
-export default App
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,49 @@
+
+const OpenAI = require('openai')
+const {Configuration, OpenAIApi} = OpenAI;
+require('dotenv').config()
+
+const openai = new OpenAI({
+    organization:process.env.ORG_ID,
+    apiKey: process.env.API_KEY
+});
+
+
+//An express server
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT || 3001
+const app=express();
+
+app.use(bodyParser.json())
+app.use(cors());
+
+
+app.post ('/', async (req,res) => {
+    const {message} = req.body;
+
+    const completion = await openai.completions.create({
+        model: "text-davinci-003",
+        prompt: `You're a machine learning strategy expert for a medium sized business. Use this input to define a 4 part strategy to utilize machine learning and AI tools to improve the effectiveness of the business and optimize COGS.  Here is the information provide:
+        ML: I am an expert in Machine Learning and can help optimize your business, and provide actionable task.
+        Person: I want to know how can minimize expenses and increase productivity
+        ML:I will help you optimize your business so you produce 20% annual savings a year on operating expenses. Provide input and I will respond with actionable results.
+        Person: ${message}?
+        ML: `,
+        max_tokens: 200,
+        temperature: 1,
+    })
+    // console.log(completion.choices[0])
+        if (completion.choices[0].text){
+            res.json({
+                message: completion.choices[0].text
+            })
+        }
+    })//end app.post
+
+
+
+app.listen(port, () => {
+    console.log(`Backend Server started on port ${port}`)
+})
